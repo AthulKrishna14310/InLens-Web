3e7c89bb548391c5da5e7757fc1044fcb4d1eb68
allenpbiju <allenpbiju1999@gmail.com>
2020-09-06 11:42:40 +0530

    Updates

---
+++ precache-manifest.504b2df2abdc1be0c21cc5f80d4babaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd8244ba4ae85212d6a46ac14beeca4",
+    "url": "/inlens-web/index.html"
+  },
+  {
+    "revision": "e0a7c18bdd8774cd4b09",
+    "url": "/inlens-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d57dd499dd4761d8eb93",
+    "url": "/inlens-web/static/js/2.129360b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inlens-web/static/js/2.129360b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a7c18bdd8774cd4b09",
+    "url": "/inlens-web/static/js/main.0fbac422.chunk.js"
+  },
+  {
+    "revision": "1329c97a25f53887acbd",
+    "url": "/inlens-web/static/js/runtime-main.f025c32f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inlens-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
